cb8e2bfd1ff71ef9122ddf2206d1b4caf81cf401
Pan Yunkui <panyunkui@gmail.com>
2019-11-09 23:52:39 -0500

    Set throw behavior.

--- src/utils/Type.ts
+++ src/utils/Type.ts
@@ -1,10 +1,12 @@
+const throwErrorObj: boolean = false;
+
 export function requireNonNull<T>(val: T | undefined | null, failureMessage: string = "Require non-null failure"): T {
-    if (val === undefined || val === null) throw new Error(failureMessage);
+    if (val === undefined || val === null) throw throwErrorObj ? new Error(failureMessage) : failureMessage;
     return val;
 }
 
 export function requireTrue(val: boolean | undefined | null, failureMessage: string = "Require true failure"): boolean {
-    if (!val) throw new Error(failureMessage);
+    if (!val) throw throwErrorObj ? new Error(failureMessage) : failureMessage;
     return val;
 }
 
@@ -13,9 +15,9 @@ export function requireFalse(val: boolean | undefined | null, failureMessage: st
 }
 
 export function requireEqual<T>(val1: T, val2: T, failureMessage: string = `Require equal failure: ${val1} vs ${val2}`): void {
-    if (val1 !== val2) throw new Error(failureMessage);
+    if (val1 !== val2) throw throwErrorObj ? new Error(failureMessage) : failureMessage;
 }
 
 export function requireNotEqual<T>(val1: T, val2: T, failureMessage: string = `Require not-equal failure: ${val1} vs ${val2}`): void {
-    if (val1 === val2) throw new Error(failureMessage);
+    if (val1 === val2) throw throwErrorObj ? new Error(failureMessage) : failureMessage;
 }
